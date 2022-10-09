240579c3b7020de743f1511af10168b6dec30ea2
jingjingxyk <zonghengbaihe521@qq.com>
2022-10-09 17:30:24 +0800

    使用tornado提供web服务

--- start-web-server.sh
+++ start-web-server.sh
@@ -7,6 +7,9 @@ __DIR__=$(
   pwd
 )
 cd ${__DIR__}
+. venv/bin/activate
+python3 -u src/NaliWrap.py
 
+exit 0
 socat -d -d -d  TCP-LISTEN:8080,fork,reuseaddr SYSTEM:"python3 src/NaliWrap.py"
 # ,crlf,crlf
